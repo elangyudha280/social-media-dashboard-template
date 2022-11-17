c5a19fe0adb598b4a277b68b477773fd4cb104a3
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-11-17 18:39:02 +0700

    mulai desain bagiang bg pattern

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@
 // import component Navbar
 import Navbar from './component/navbar.jsx'
 
+// import component bg pattern
 
 
 function App() {
   return (
       <>
-      
       {/* component navbar */}
        <Navbar/>  
       </>
