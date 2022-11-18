c739370cd831555f2b1d982579e9613b7f694111
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-11-18 19:58:06 +0700

    Selesai buat data cards

--- src/component/socialMediaContainer.jsx
+++ src/component/socialMediaContainer.jsx
@@ -25,41 +25,64 @@ const CardItem = ({totalFollowers,userNameSocialMedia,countFollowers,urlImg,text
 
 //   component SocialMediaContainer 
 const SocialMediaContainer = ()=>{
+
+    let dataCards = [
+        {
+            id:1,
+            userNameSocialMedia:'@nathanf',
+            totalFollowers:'1987',
+            textFollowers:'followers',
+            urlImg:'assets/images/icon-facebook.svg',
+            countImg:'assets/images/icon-up.svg',
+            countFollowers:'12 today'
+        },
+        {
+            id:2,
+            userNameSocialMedia:'@nathanf',
+            totalFollowers:'1044',
+            textFollowers:'followers',
+            urlImg:'assets/images/icon-twitter.svg',
+            countImg:'assets/images/icon-up.svg',
+            countFollowers:'99 today'
+        },
+        {
+            id:3,
+            userNameSocialMedia:'@realnathanf',
+            totalFollowers:'11k',
+            textFollowers:'followers',
+            urlImg:'assets/images/icon-instagram.svg',
+            countImg:'assets/images/icon-up.svg',
+            countFollowers:'1099 today'
+        },
+        {
+            id:4,
+            userNameSocialMedia:'Nathan F.',
+            totalFollowers:'8239',
+            textFollowers:'subscribers',
+            urlImg:'assets/images/icon-youtube.svg',
+            countImg:'assets/images/icon-down.svg',
+            countFollowers:'144 today'
+        },
+    ]
+
     return (
         <section className="social-media-container container">
             <div className="card-container">
-            <CardItem 
-            textFollowers="followers"
-            urlImg="assets/images/icon-facebook.svg"
-             userNameSocialMedia="@nathanf"
-             totalFollowers="1987"
-             countImg="assets/images/icon-up.svg"
-             countFollowers="12 today"
-            />
-             <CardItem 
-                     textFollowers="followers"
-            urlImg="assets/images/icon-twitter.svg"
-             userNameSocialMedia="@nathanf"
-             totalFollowers="1044"
-             countImg="assets/images/icon-up.svg"
-             countFollowers="99 today"
-            />
-             <CardItem 
-                     textFollowers="followers"
-            urlImg="assets/images/icon-instagram.svg"
-             userNameSocialMedia="@realnathanf"
-             totalFollowers="11k"
-             countImg="assets/images/icon-up.svg"
-             countFollowers="1099 today"
-            />
-            <CardItem 
-                    textFollowers="subscribers"
-            urlImg="assets/images/icon-youtube.svg"
-             userNameSocialMedia="Nathan F."
-             totalFollowers="8239"
-             countImg="assets/images/icon-down.svg"
-             countFollowers="144 today"
-            />
+            {
+                dataCards.map(e =>{
+                    return (
+                        <CardItem 
+                        key={e.id}
+                        userNameSocialMedia={e.userNameSocialMedia}
+                        totalFollowers={e.totalFollowers}
+                        textFollower={e.textFollowers}
+                        urlImg={e.urlImg}
+                        countImg={e.countImg}
+                        countFollowers={e.countFollowers}
+                        />
+                    )
+                })
+            }             
             </div>
         </section>
     )
